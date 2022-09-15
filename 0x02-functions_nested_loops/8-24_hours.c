ee7832ca1259b356753ea4a9886406425e44d575
Alipo Mpita <alipompita@gmail.com>
2022-09-15 04:41:30 +0200

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,6 +2,18 @@
 #include "main.h"
 
 /**
+ * get_first - gets first digit
+ * @n: candidate
+ * Return: first digit as int
+ */
+int get_first(int n)
+{
+	while (n > 10)
+		n = n / 10;
+	return (n);
+}
+
+/**
  * jack_bauer - prints jack bauer time
  */
 void jack_bauer(void)
@@ -13,9 +25,35 @@ void jack_bauer(void)
 	{
 		for (m = 0; m <= 59; m++)
 		{
-			_putchar('0' + h);
+			if (h >= 10)
+			{
+				int firsth, lasth;
+
+				firsth = get_first(h);
+				lasth = h % 10;
+				_putchar('0' + firsth);
+				_putchar('0' + lasth);
+			} else
+			{
+				_putchar('0' + 0);
+				_putchar('0' + h);
+			}
+
 			_putchar(':');
-			_putchar('0' + m);
+
+			if (m >= 10)
+			{
+				int firstm, lastm;
+
+				firstm = get_first(m);
+				lastm = m % 10;
+				_putchar('0' + firstm);
+				_putchar('0' + lastm);
+			} else
+			{
+				_putchar('0' + 0);
+				_putchar('0' + m);
+			}
 			_putchar('\n');
 		}
 	}
