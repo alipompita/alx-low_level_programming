ffc3c75908f999ad4ea29da700e9fb60dd2706ed
Alipo Mpita <alipompita@gmail.com>
2022-09-15 03:57:15 +0200

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_first - get first digit of number
+ * @n: number
+ * Return: int first digit
+ */
+int get_first(int n)
+{
+	while (n > 10)
+	{
+		n = n / 10;
+	}
+
+	return (n);
+}
+
+/**
+ * times_table - prints 9 times table
+ */
+void times_table(void)
+{
+	int i;
+	int c;
+	int result;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 0; c <= 9; c++)
+		{
+			result = i * c;
+			if (result < 10)
+			{
+				_putchar('0' + result);
+			} else
+			{
+				int first = get_first(result);
+				int last = result % 10;
+
+				_putchar('0' + first);
+				_putchar('0' + last);
+			}
+
+			if (c < 9)
+				_putchar(',');
+			else
+				_putchar('\n');
+		}
+	}
+}
