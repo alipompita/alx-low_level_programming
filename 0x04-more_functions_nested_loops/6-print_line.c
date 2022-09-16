6e0c22101d1a04730a4aeba4a8bbbca67f624877
Alipo Mpita <alipompita@gmail.com>
2022-09-16 04:35:25 +0200

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - prints line
+ * @n: size of line
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
