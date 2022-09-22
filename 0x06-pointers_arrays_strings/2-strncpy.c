4dae179ddbdb584f847b898099246a7326e7682a
Alipo Mpita <alipompita@gmail.com>
2022-09-22 21:35:12 +0000

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,14 +16,11 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i >= len)
+		if (i > len)
 			*(dest + i) = '\0';
 		else
 			*(dest + i) = src[i];
 	}
-
-	if (i >= len)
-		*(dest + i) = '\0';
-
+	
 	return (dest);
 }
