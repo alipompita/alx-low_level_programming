26ce5b29afdaa2ea8cc580911de75602106acfe0
Alipo Mpita <alipompita@gmail.com>
2022-09-22 19:23:41 +0000

    concatenet

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,15 +11,16 @@ char *_strcat(char *dest, char *src)
 	int i, c;
 
 	i = 0;
-	while (dest[i] != '\n')
+	while (dest[i] != '\0')
 		i++;
 
 	c = 0;
-	while (src[c] != '\n')
+	while (src[c] != '\0')
 	{
 		*(dest + i) = src[c];
 		i++;
 		c++;
 	}
+	*(dest + i) = '\0';
 	return (dest);
 }
