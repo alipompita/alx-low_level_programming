fa8a2a90c6e8b9c20d5a858627111ffff69c02cf
Alipo Mpita <alipompita@gmail.com>
2022-09-22 22:59:29 +0000

    comparing files atempt

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,10 +3,16 @@
 /**
  * _strcmp - compares 2 strings
  * @s1: string 1
- * @s2: String 2
- * Return: int difference
+ * @s2: string 2
+ * Return: diference
  */
 int _strcmp(char *s1, char *s2)
 {
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++, s2++;
+	}
 	return (*s1 - *s2);
 }
