d89b63df8645c3b433097273f5fd5e64d0439406
Alipo Mpita <alipompita@gmail.com>
2022-09-23 01:10:49 +0000

    1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,32 +1,22 @@
 #include "main.h"
 
 /**
- * leet - encodes strings
- * @s: string
- * Return: replaced String
+ * leet - encode string
+ * @s: candidate string
+ * Return: string address
  */
 char *leet(char *s)
 {
-	int i;
+	int i = 0, j;
+	char subs[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
+	     str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 
-	i = 0;
-	while (s[i] != '\0')
+	while (s[i])
 	{
-		char l = s[i];
-
-		if (l == 'a' || l == 'A')
-			*(s + i) = '4';
-		else if (l == 'e' || l == 'E')
-			*(s + i) = '3';
-		else if (l == 'o' || l == 'O')
-			*(s + i) = '0';
-		else if (l == 't' || l == 'T')
-			*(s + i) = '7';
-		else if (l == 'l' || l == 'L')
-			*(s + i) = '1';
-
+		for (j = 0; j < 10; j++)
+			if (s[i] == str[j])
+				s[i] = subs[j];
 		i++;
 	}
-
 	return (s);
 }
