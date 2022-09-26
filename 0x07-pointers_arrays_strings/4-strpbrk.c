b736aef5119ae5adc9a39b9dc6dd7dafe3b30a36
Alipo Mpita <alipompita@gmail.com>
2022-09-26 13:55:13 +0000

    fix betty

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strpbrk - find first occurrence of byte 
+ * _strpbrk - find first occurrence of byte
  * in a string
  * @s: source string
  * @accept: candidate string
