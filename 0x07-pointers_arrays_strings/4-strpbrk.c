a7e7b51188eaf18ea01e34018a9790e7df1ee361
Alipo Mpita <alipompita@gmail.com>
2022-09-26 13:52:54 +0000

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strpbrk - find first occurrence of byte 
+ * in a string
+ * @s: source string
+ * @accept: candidate string
+ * Return: address of byte
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *origin = accept;
+
+	while (*s)
+	{
+		while (*accept)
+		{
+			if (*accept == *s)
+				return (s);
+			accept++;
+		}
+		accept = origin;
+		s++;
+	}
+
+	return (NULL);
+
+}
