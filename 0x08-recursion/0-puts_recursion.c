a763085ea71e85e992562029d2c817c6ecda5755
Alipo Mpita <alipompita@gmail.com>
2022-09-28 22:31:11 +0200

    recursive puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * void _puts_recursion - prints a string with rucursion
+ * @s: string input
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+}
