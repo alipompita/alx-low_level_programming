f0c6cf12d474b11280908be056a206edf742ce2f
Alipo Mpita <alipompita@gmail.com>
2022-10-21 03:08:13 +0200

    constructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main_constructor(void) __attribute__((constructor));
+/**
+ * main_constructor - prints text before running main
+ */
+void main_constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
