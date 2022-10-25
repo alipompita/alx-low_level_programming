d47a26c7fa02550e8e9e5cb2764fcff565454652
Alipo Mpita <alipompita@gmail.com>
2022-10-25 07:33:42 +0000

    count elements in list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,9 +2,19 @@
 #include <stdio.h>
 
 /**
- *
+ * listint_len - counts elements of a list
+ * @h: pointer to first node
+ * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
+	unsigned int nodes = 0;
 
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
 }
