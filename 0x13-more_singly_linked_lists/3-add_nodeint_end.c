f9b4eddf4223ccdaa16955559d17493cd1eb51d9
Alipo Mpita <alipompita@gmail.com>
2022-10-25 12:42:07 +0000

    fix betty on 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *last, *new = malloc(sizeof(listint_t));
 
 	if (new == NULL)
-		return NULL;
+		return (NULL);
 
 	new->n = n;
 	new->next = NULL;
